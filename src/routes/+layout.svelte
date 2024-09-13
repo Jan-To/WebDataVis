46589ac0b6aece6854d4e231960bac05e203f3cf
Jan-Tobias Sohns <j_sohns12@cs.uni-kl.de>
2024-09-13 18:15:05 +0200

    first attempt of remodel to try to switch to full svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<nav>
+	<a href="/">About</a>
+	<a href="/htmlcssjs">HTML, CSS and Javascript</a>
+</nav>
+
+<slot></slot>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<h1>Implementing data visualisations using Svelte</h1>
+
+<p>
+  This course is adapted from the course material of Prof. Dr. Jan Aerts at the KU Leuven. 
+  The original material can be found at <a href="http://vda-lab.gitlab.io/datavis-technologies">http://vda-lab.gitlab.io/datavis-technologies</a>.
+</p>
+
+<p>
+  <a href="/htmlcssjs">HTML, CSS and Javascript</a>
+</p>
